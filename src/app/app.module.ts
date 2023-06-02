0da0e63c65093371ac8aa2d1df138f7cc6e47ecf
wasimnahed <Wasim.Nahed@softobiz.com>
2023-06-02 16:17:27 +0530

    todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentComponent,
     ChildComponent,
+    TodoListComponent,
     
   ],
   imports: [
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+  list: any[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+ 
+  addTask(item: string) {
+    // console.log(item);
+    this.list.push(
+      {
+        id: this.list.length,
+        name: item
+      }
+    )
+    console.log(this.list);
+  }
+
+  removeTask(id: number) {
+    console.log(id);
+    this.list=this.list.filter(item=>item.id!==id)
+
+  }
+
+}
