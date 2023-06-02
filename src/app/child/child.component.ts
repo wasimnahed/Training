f52817306b34235a3563b41a1552e30ce2bab12c
wasimnahed <Wasim.Nahed@softobiz.com>
2023-06-02 14:38:31 +0530

    some changes

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -6,10 +6,10 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
-
   @Input() myinput: string | undefined;
-  @Output() myOutput: EventEmitter<string> = new EventEmitter();
-  outputstring = 'It is child component';
+  @Output() myOutput: EventEmitter<Object> = new EventEmitter<object>();
+  // outputstring = 'It is child component';
+  outputstring ={ name: 'Wasim Nahed', age: 26 };
   constructor() { }
 
   ngOnInit(): void {
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
@@ -9,14 +9,17 @@ export class ParentComponent implements OnInit {
 
   inputstring:string="It is parent component";
   apiMenu: string ='';
+  apiMenu2: string ='';
   constructor() { }
 
   ngOnInit(): void {
   }
 
   getData(value:any){
-    this.apiMenu=value
-    console.log(this.apiMenu);
+    this.apiMenu=value.name;
+    this.apiMenu2=value.age;
+    console.log('The name is ',this.apiMenu);
+    console.log('and age is',this.apiMenu2);
     
   }
 
